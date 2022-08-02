7f32ac33073c227468491cbf689b22f517837203
Chandra Perdiansyah <66311673+cperdiansyah@users.noreply.github.com>
2022-08-02 15:47:09 +0700

    feat : add users controller and refreshToken controller

---
+++ models/UserModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: [true, 'Email already exists'],
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model('User', userSchema)
